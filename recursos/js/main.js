7300d452786cd00705478bb3e19d7ac64bc1a985
Elias David Garcia Hernandez <edgh9508@gmail.com>
2018-03-27 11:12:59 -0600

    3 commit fb

--- recursos/js/main.js
+++ recursos/js/main.js
@@ -63,7 +63,7 @@ var a = "emergentes fb";
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?fields=id,name,context', function(response) {
+    FB.api('/me?fields=id,name,context,email', function(response) {
       console.log(response.context);
       listaAmigos(response);
       console.log('Successful login for: ' + response.name);
